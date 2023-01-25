b9ab3208c77c8cf70fde6ee7b9f6c32b50407f09
Local <Local@admin.me>
2023-01-25 19:40:32 +0600

    Intercative and Main cpp files added.

--- LawGitEst.cpp
+++ LawGitEst.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <list>
+#include "Act.h"
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    std::string READ_FILE{ "read.csv" };
+    std::string WRITE_FILE{ "write.csv" };
+
+    Act fiction("УК РК;385;Подделка документов;Штраф 2000 МРП / КПЗ 2 года;");
+    fiction.manual_list(fiction);
+
+    fiction.Act_file(READ_FILE);
+    fiction.list_show();
+    fiction.write(WRITE_FILE);
 
     return 0;
 }
